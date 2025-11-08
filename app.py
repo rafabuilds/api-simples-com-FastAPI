5c88ba9517eb491d8327851ec3260268bf768b65
rafabuilds <rafaelcontreira.dev@hotmail.com>
2025-11-08 00:53:10 -0300

    criando nossa variável global PRODUTOS

--- app.py
+++ app.py
@@ -2,6 +2,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root(): 
-    return {"message": "Hello, World!"}
+PRODUTOS = [
+    {
+        "id": 1,
+        "sku": "FIT-BIO-000",
+        "nome": "Fitmass Bioscan",
+        "descricao": "Balança de bioimpedância com tela de x polegadas.",
+        "preco": 8599.00, # Preços devem ser números, definidos como float ou decimal.
+        "tipo": "Hardware",
+        "recorrencia": "Não se aplica",
+        "ativo": True
+    },
+    {
+        "id": 2,
+        "sku": "FIT-SYS-PRO-000",
+        "nome": "Fitmass System - Plano Pro (Academia)", # Ou para profissionais como nutricionais, personal trainer, etc...
+        "descricao": "App de gestão de alunos para personal trainers (até 500 alunos).",
+        "preco": 299.90,
+        "tipo": "Software",
+        "recorrencia": "Mensal", # Pode ser trimestral ou anual, também.
+        "ativo": True 
+    }
+]
+
+# #@app.get("/")
+# async def root(): 
+#     """this is the root path."""
+#     return {"message": "Hello, World!"}
+
