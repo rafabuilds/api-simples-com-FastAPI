92e205acfdd5547763aaa6967019141576a99925
rafabuilds <rafaelcontreira.dev@hotmail.com>
2025-11-08 23:17:33 -0300

    Hoje (08/11/2025) criamos nosso Get para produtos(listar e obter produtos) e também criamos o nosso Post para criar novos Produtos.
    Importamos pacotes como typing e pydantic para tornar opcional o valor "ativo" e para deixar em um formato padrão nossos valores criamos a classe Produto, para quando for criado novos produtos.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI 
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 PRODUTOS = [
     {
         "id": 1,
-        "sku": "FIT-BIO-000",
+        "codigo_produto": "FIT-BIO-000",
         "nome": "Fitmass Bioscan",
         "descricao": "Balança de bioimpedância com tela de x polegadas.",
         "preco": 8599.00, # Preços devem ser números, definidos como float ou decimal.
@@ -15,18 +18,42 @@ PRODUTOS = [
     },
     {
         "id": 2,
-        "sku": "FIT-SYS-PRO-000",
-        "nome": "Fitmass System - Plano Pro (Academia)", # Ou para profissionais como nutricionais, personal trainer, etc...
-        "descricao": "App de gestão de alunos para personal trainers (até 500 alunos).",
-        "preco": 299.90,
+        "codigo_produto": "FIT-POC-001",
+        "nome": "Fitmass Pocket - Personal", # Ou para Academias.
+        "descricao": "App de gestão de alunos para personal trainers (até 100 alunos) ou nutricionistas.",
+        "preco": 49.90,
         "tipo": "Software",
         "recorrencia": "Mensal", # Pode ser trimestral ou anual, também.
         "ativo": True 
     }
 ]
 
-# #@app.get("/")
-# async def root(): 
-#     """this is the root path."""
-#     return {"message": "Hello, World!"}
+class Produto(BaseModel):
+    """Classe Produto."""
+    id: int
+    codigo_produto: str
+    nome: str
+    descricao: str
+    preco: float
+    tipo: str
+    recorrencia: str
+    ativo: Optional[bool] = True
 
+@app.get("/produtos", tags=["produtos"])
+def listar_produtos() -> list:
+    """Listar Produtos."""
+    return PRODUTOS
+     
+@app.get("/produtos/{produto_id}", tags=["produtos"])
+def obter_produtos(produto_id: int) -> dict:
+    """Obter Produtos."""
+    for produto in PRODUTOS:
+        if produto["id"] == produto_id:
+            return produto
+    return None
+
+@app.post("/produtos", tags=["produtos"])
+def criar_produto(produto: Produto) -> dict:
+    """Criar Produtos."""
+    PRODUTOS.append(produto)
+    return produto
